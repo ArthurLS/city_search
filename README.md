54fdcabf1d6623bd4c146447decd45422733d73d
Arthur LE SAINT <arthur.lesaint@hotmail.fr>
2021-11-23 16:26:21 +0100

    Readme, better sort.

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 # City Search
 
 Graneet test project. 3 algo questions and a mini city search project.
+
+## Client and server setup
+
+Run these commands in both server and client folders to install the modules and start up the project :
+```
+npm i
+npm start
+```
--- server/src/app.service.ts
+++ server/src/app.service.ts
@@ -9,14 +9,18 @@ export class AppService {
     const resCities = cities
       .filter((city) => {
         if (code) {
-          if (name) {
-            return city.codePostal.includes(code) && city.nomCommune.normalize().includes(name.normalize());
-          }
           return city.codePostal.includes(code);
         } else if (name) {
-          return city.nomCommune.normalize().includes(name.normalize());
+          return city.nomCommune.toLocaleLowerCase().substring(0, name.length) === name.toLocaleLowerCase();
         }
         return true; 
+      })
+      .sort((a, b) =>{
+        if (a.nomCommune < b.nomCommune)
+          return -1;
+        if ( a.nomCommune > b.nomCommune)
+          return 1;
+        return 0;
       })
       .filter((city, index) => index < 100)
 
